5436a66cb92a5c94957fea6de1f9f6f0b700fd2c
wknowpark797 <wknowpark797@gmail.com>
2023-09-07 12:10:51 +0900

    setURL, fetchData 메서드 타입 지정

--- js/flickr.js
+++ js/flickr.js
@@ -1,3 +1,12 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
     if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
     if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
@@ -23,7 +32,7 @@ class MyFlickr {
         this.createInit();
         // this.enableInterest && this.createBtnSet();
         // this.enableSearch && this.createSearchBox();
-        // this.fecthData(this.setURL('interest'));
+        this.fecthData(this.setURL('interest'));
     }
     createInit() {
         var _a, _b;
@@ -40,6 +49,36 @@ class MyFlickr {
         // 인스턴스에 값을 넘기려면 멤버변수에 등록이 되어야하므로 멤버변수를 추가해주고 타입 지정
         this.wrap = wrap;
         this.loading = img;
+    }
+    // 두번째 인수값에 객체를 전달받을 수 있지만 값이 없는 경우를 위해 옵셔널 체이닝 처리
+    setURL(type, opt) {
+        const baseURL = `https://www.flickr.com/services/rest/?format=json&nojsoncallback=1&api_key=${this.api_key}&per_page=${this.num}&method=`;
+        const method_interest = 'flickr.interestingness.getList';
+        const method_user = 'flickr.people.getPhotos';
+        const method_search = 'flickr.photos.search';
+        if (type === 'interest')
+            return `${baseURL}${method_interest}`;
+        if (type === 'search')
+            return `${baseURL}${method_search}&tags=${opt}`;
+        if (type === 'user')
+            return `${baseURL}${method_user}&user_id=${opt}`;
+    }
+    // url값으로 문자를 지정하되 값이 없을 때를 고려하여 undefined 추가
+    fecthData(url) {
+        var _a, _b, _c, _d;
+        return __awaiter(this, void 0, void 0, function* () {
+            (_a = this.loading) === null || _a === void 0 ? void 0 : _a.classList.remove('off');
+            (_b = this.wrap) === null || _b === void 0 ? void 0 : _b.classList.remove('on');
+            // 인수에 값을 전달할 때 값이 없을수도 있는 경우 대체값을 지정
+            const res = yield fetch(url || '');
+            const json = yield res.json();
+            const items = json.photos.photo;
+            if (items.length === 0) {
+                (_c = this.loading) === null || _c === void 0 ? void 0 : _c.classList.add('off');
+                (_d = this.wrap) === null || _d === void 0 ? void 0 : _d.classList.add('on');
+                return alert('해당 검색어의 결과이미지가 없습니다.');
+            }
+        });
     }
 }
 _MyFlickr_defOpt = new WeakMap();
--- js/flickr.ts
+++ js/flickr.ts
@@ -43,7 +43,7 @@ class MyFlickr {
 		this.createInit();
 		// this.enableInterest && this.createBtnSet();
 		// this.enableSearch && this.createSearchBox();
-		// this.fecthData(this.setURL('interest'));
+		this.fecthData(this.setURL('interest'));
 	}
 
 	createInit() {
@@ -61,5 +61,34 @@ class MyFlickr {
 		// 인스턴스에 값을 넘기려면 멤버변수에 등록이 되어야하므로 멤버변수를 추가해주고 타입 지정
 		this.wrap = wrap;
 		this.loading = img;
+	}
+
+	// 두번째 인수값에 객체를 전달받을 수 있지만 값이 없는 경우를 위해 옵셔널 체이닝 처리
+	setURL(type: string, opt?: Object) {
+		const baseURL = `https://www.flickr.com/services/rest/?format=json&nojsoncallback=1&api_key=${this.api_key}&per_page=${this.num}&method=`;
+		const method_interest = 'flickr.interestingness.getList';
+		const method_user = 'flickr.people.getPhotos';
+		const method_search = 'flickr.photos.search';
+
+		if (type === 'interest') return `${baseURL}${method_interest}`;
+		if (type === 'search') return `${baseURL}${method_search}&tags=${opt}`;
+		if (type === 'user') return `${baseURL}${method_user}&user_id=${opt}`;
+	}
+
+	// url값으로 문자를 지정하되 값이 없을 때를 고려하여 undefined 추가
+	async fecthData(url: string | undefined) {
+		this.loading?.classList.remove('off');
+		this.wrap?.classList.remove('on');
+
+		// 인수에 값을 전달할 때 값이 없을수도 있는 경우 대체값을 지정
+		const res = await fetch(url || '');
+		const json = await res.json();
+		const items = json.photos.photo;
+		if (items.length === 0) {
+			this.loading?.classList.add('off');
+			this.wrap?.classList.add('on');
+			return alert('해당 검색어의 결과이미지가 없습니다.');
+		}
+		// this.createList(items);
 	}
 }
