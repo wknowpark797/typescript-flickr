800b845c69188094f8f7094659529c5d1aaea8fd
wknowpark797 <wknowpark797@gmail.com>
2023-09-07 11:58:17 +0900

    MyFlickr 클래스 생성자 및 createInit 메서드 타입 지정

--- js/flickr.js
+++ js/flickr.js
@@ -1,16 +1,45 @@
-class ChangeColor {
-    // 생성자 호출시 selector에 전달되는 값은 문자열
-    constructor(selector, color) {
-        this.selector = selector;
-        this.color = color;
-        this.bindingEvent(this.color);
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
+};
+var _MyFlickr_defOpt;
+class MyFlickr {
+    constructor(selector, opt) {
+        // 기본값
+        _MyFlickr_defOpt.set(this, { num: 50, enableSearch: true, enableInterest: true });
+        this.frame = document.querySelector(selector);
+        this.wrap = null;
+        this.loading = null;
+        this.opt = Object.assign(Object.assign({}, __classPrivateFieldGet(this, _MyFlickr_defOpt, "f")), opt);
+        this.api_key = this.opt.api_key;
+        this.myId = this.opt.myId;
+        this.num = this.opt.num;
+        this.enableSearch = this.opt.enableSearch;
+        this.enableInterest = this.opt.enableInterest;
+        this.bindingEvent();
     }
-    bindingEvent(color) {
-        var _a;
-        (_a = this.selector) === null || _a === void 0 ? void 0 : _a.addEventListener('click', (e) => {
-            // e.target 객체를 타입스크립트가 인지하지 못하므로 명시적으로 타입 지정
-            const eventTarget = e.target;
-            eventTarget.style.background = color;
-        });
+    bindingEvent() {
+        this.createInit();
+        // this.enableInterest && this.createBtnSet();
+        // this.enableSearch && this.createSearchBox();
+        // this.fecthData(this.setURL('interest'));
+    }
+    createInit() {
+        var _a, _b;
+        const img = document.createElement('img');
+        const src = document.createAttribute('src');
+        const wrap = document.createElement('ul');
+        src.value = 'img/loading.gif';
+        img.setAttributeNode(src);
+        wrap.classList.add('wrap');
+        img.classList.add('loading');
+        (_a = this.frame) === null || _a === void 0 ? void 0 : _a.append(wrap);
+        (_b = this.frame) === null || _b === void 0 ? void 0 : _b.append(img);
+        // wrap, loading 값은 createInit 메서드가 호출되어야 만들어지는 값이지만
+        // 인스턴스에 값을 넘기려면 멤버변수에 등록이 되어야하므로 멤버변수를 추가해주고 타입 지정
+        this.wrap = wrap;
+        this.loading = img;
     }
 }
+_MyFlickr_defOpt = new WeakMap();
--- js/flickr.ts
+++ js/flickr.ts
@@ -1,20 +1,65 @@
-class ChangeColor {
-	// 인스턴스 객체에 넘어가는 값은 DOM으로 변환된 객체 혹은 null
-	selector: HTMLElement | null;
-	color: string;
+interface DefOpt {
+	num: number;
+	enableSearch: boolean;
+	enableInterest: boolean;
+}
+interface Opt {
+	api_key: string;
+	num: number;
+	myId: string;
+	enableSearch: boolean;
+	enableInterest: boolean;
+}
 
-	// 생성자 호출시 selector에 전달되는 값은 문자열
-	constructor(selector: HTMLElement, color: string) {
-		this.selector = selector;
-		this.color = color;
-		this.bindingEvent(this.color);
+class MyFlickr {
+	// 기본값
+	#defOpt: DefOpt = { num: 50, enableSearch: true, enableInterest: true };
+
+	// 멤버변수 지정
+	frame: HTMLElement | null;
+	opt: Opt;
+	api_key: string;
+	myId: string;
+	num: number;
+	enableSearch: boolean;
+	enableInterest: boolean;
+	wrap: HTMLElement | null;
+	loading: HTMLImageElement | null;
+
+	constructor(selector: string, opt: Opt) {
+		this.frame = document.querySelector(selector);
+		this.wrap = null;
+		this.loading = null;
+		this.opt = { ...this.#defOpt, ...opt };
+		this.api_key = this.opt.api_key;
+		this.myId = this.opt.myId;
+		this.num = this.opt.num;
+		this.enableSearch = this.opt.enableSearch;
+		this.enableInterest = this.opt.enableInterest;
+		this.bindingEvent();
 	}
 
-	bindingEvent(color: string) {
-		this.selector?.addEventListener('click', (e: Event) => {
-			// e.target 객체를 타입스크립트가 인지하지 못하므로 명시적으로 타입 지정
-			const eventTarget = e.target as HTMLElement;
-			eventTarget.style.background = color;
-		});
+	bindingEvent() {
+		this.createInit();
+		// this.enableInterest && this.createBtnSet();
+		// this.enableSearch && this.createSearchBox();
+		// this.fecthData(this.setURL('interest'));
+	}
+
+	createInit() {
+		const img = document.createElement('img');
+		const src = document.createAttribute('src');
+		const wrap = document.createElement('ul');
+		src.value = 'img/loading.gif';
+
+		img.setAttributeNode(src);
+		wrap.classList.add('wrap');
+		img.classList.add('loading');
+		this.frame?.append(wrap);
+		this.frame?.append(img);
+		// wrap, loading 값은 createInit 메서드가 호출되어야 만들어지는 값이지만
+		// 인스턴스에 값을 넘기려면 멤버변수에 등록이 되어야하므로 멤버변수를 추가해주고 타입 지정
+		this.wrap = wrap;
+		this.loading = img;
 	}
 }
